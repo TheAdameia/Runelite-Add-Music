5c1a80fb5e56a34209b58ac80ebb0a103bbe9663
Adameia Ramsey <adameiaproductions@gmail.com>
2026-09-08 17:55:20 -0500

    building primitive panel

---
+++ src/main/java/com/addmusic/AddMusicPanel.java
@@ -0,0 +1,15 @@
+package com.addmusic;
+
+import javax.swing.*;
+
+public class AddMusicPanel extends JPanel
+{
+    public AddMusicPanel()
+    {
+        super();
+
+        JLabel label = new JLabel("Hello from AddMusic!");
+
+        add(label);
+    }
+}
