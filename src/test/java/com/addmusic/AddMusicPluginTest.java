1bbe6e12f26cc0b282146f767536489adf979625
Adameia Ramsey <adameiaproductions@gmail.com>
2026-09-07 19:56:21 -0500

    renaming boilerplate

--- src/test/java/com/addmusic/AddMusicPluginTest.java
+++ src/test/java/com/addmusic/AddMusicPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.addmusic;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class AddMusicPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(AddMusicPlugin.class);
 		RuneLite.main(args);
 	}
 }
